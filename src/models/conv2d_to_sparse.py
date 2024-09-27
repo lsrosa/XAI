b78461d29b84226e3a0efef1b7d4b2b21b34818f
leandro <leandro.desouzarosa@unibo.it>
2024-09-27 19:12:02 +0200

    working

--- src/models/conv2d_to_sparse.py
+++ src/models/conv2d_to_sparse.py
@@ -6,52 +6,28 @@ from tqdm import tqdm
 from itertools import product
 import torch
 
-def conv2d_to_sparse(input_shape, weight_tensor, bias, stride=(1, 1), padding=(1, 1)):
-    ''' 
-    Convert a 2D convolution operation represented by dense weight and bias tensors
-    into a sparse matrix representation using Compressed Sparse Row (CSR) format.
+def conv2d_to_sparse(input_shape, weight_tensor, bias, stride=(1, 1), padding=(0, 0)):
 
-    Args:
-    - input_shape (tuple): The shape of the input tensor in the format (Cin, Hin, Win),
-      where Cin is the number of input channels, Hin is the height of the input,
-      and Win is the width of the input.
-    - weight_tensor (numpy.ndarray): The weight tensor of the convolution layer
-      with shape (Cout, Cin, Kh, Kw), where Cout is the number of output channels,
-      Kh is the kernel height, and Kw is the kernel width.
-    - bias (numpy.ndarray): The bias tensor with shape (Cout,) representing the biases
-      for each output channel.
-    - stride (tuple): The stride of the convolution operation in the format (stride_h, stride_w).
-    - padding (tuple): The padding applied to the input in the format (pad_h, pad_w).
-
-    Returns:
-    - csr_mat (scipy.sparse.csr_matrix): The sparse matrix representation of the convolution
-      operation in Compressed Sparse Row (CSR) format.
-    '''
     Cin, Hin, Win = input_shape
-    print('input shape: ', input_shape, Cin, Hin, Win) 
     Cout = weight_tensor.shape[0]
-    print('Cout: ', Cout)
     kernel_shape = tuple(weight_tensor.shape[2:])
-    print('kernel shape: ', kernel_shape)
     kernel = weight_tensor.detach().numpy()
     bias = bias.detach().numpy()
-    print('kernel: ', kernel)
 
-    Hout = int(np.floor((Hin + 2*padding[-2] - (kernel_shape[-2] - 1) -1)/stride[-2] + 1))
-    Wout = int(np.floor((Win + 2*padding[-1] - (kernel_shape[-1] - 1) -1)/stride[-1] + 1))
-    
+    Hout = int(np.floor((Hin + 2*padding[0] - (kernel_shape[0] - 1) -1)/stride[0] + 1))
+    Wout = int(np.floor((Win + 2*padding[1] - (kernel_shape[1] - 1) -1)/stride[1] + 1))
+
     rows = []
     cols = []
     data = []
 
     for f, i, j, c in tqdm(product(range(Cout), range(Hout), range(Wout), range(Cin))):
-        row_start = f * Hout * Wout + i * Wout + j 
+        row = f * Hout * Wout + i * Wout + j 
         col_start = c * Hin * Win 
         weight = kernel[f, c]
         
         for m in range(kernel_shape[0]):
             for n in range(kernel_shape[1]):
-                row = row_start
                 col = col_start + ((m * Win + n + i * stride[1] * Win + j * stride[0]) % (Win * Hin))
 
                 rows.append(row)
@@ -68,14 +44,62 @@ def conv2d_to_sparse(input_shape, weight_tensor, bias, stride=(1, 1), padding=(1
                 data.append(bias[f])
     
     data, rows, cols = np.array(data), np.array(rows), np.array(cols)
-    print('data len: ', len(data)) 
 
     # convert to csr
-    csr_mat = csr_matrix((data, (rows, cols)), shape=(Cout * Hout * Wout, Cin * Hin * Win + 1))
+    m = csr_matrix((data, (rows, cols)), shape=(Cout * Hout * Wout, Cin * Hin * Win + 1))
+    csr_mat = torch.sparse_csr_tensor(m.indptr, m.indices, m.data, size=m.shape)
+    return m, csr_mat
+
+def conv2d_to_sparse2(input_shape, weight, bias, stride=(1, 1), padding=(0, 0), dilation=(1,1)):
+
+    Cin, Hin, Win = input_shape
+    #print('input shape: ', input_shape, Cin, Hin, Win) 
+    Cout = weight.shape[0]
+    #print('Cout: ', Cout)
+    Hk = weight.shape[2]
+    Wk = weight.shape[3]
+    #print('kernel shape: ', Hk, Wk)
+    kernel = weight
+    #print('kernel: ', kernel)
+
+    Hout = int(np.floor((Hin + 2*padding[0] - dilation[0]*(Hk - 1) -1)/stride[0] + 1))
+    Wout = int(np.floor((Win + 2*padding[1] - dilation[1]*(Wk - 1) -1)/stride[1] + 1))
+    #print('hout, wout: ', Hout, Wout)
     
-    print(np.linalg.norm(x1-x2), np.linalg.norm(x1-x3) )
+    shape_out = torch.Size((Cout*Hout*Wout, Cin*Hin*Win+1))
+    print('shape out: ', shape_out)
+    
+    crow = (torch.linspace(0, shape_out[0], shape_out[0]+1)*(Hk*Wk*Cin+1)).int()
+    print('crow: ', crow)
+    nnz = crow[-1]
+    print('nnz: ', nnz, Cout*Hout*Wout)
+    
+    # getting columns
+    cols = []
+    for f, i, j, c in tqdm(product(range(Cout), range(Hout), range(Wout), range(Cin))):
+        col_start = c * Hin * Win 
+        
+        for m in range(Hk):
+            for n in range(Wk):
+                col = col_start + ((m * Win + n + i * stride[1] * Win + j * stride[0]) % (Win * Hin))
+                cols.append(col)
+    
+    # add bias as the last column                    
+    for f in tqdm(range(Cout)):
+        for i in range(Hout):
+            for j in range(Wout):
+                cols.append(Cin * Hin * Win)
+    
+    cols = torch.tensor(cols)
 
-    #_csr_mat = torch.sparse_csr_tensor(rows, cols, data)
-    return csr_mat#, _csr_mat
+    # data is the kernel values, plus bias
+    data = torch.zeros(Cout*Hout*Cout, Cin*Hk*Wk+1)
+    for cout in range(Cout):
+        k = kernel[cout]
+        _d = torch.hstack((k.flatten(), bias[cout]))
+        for i, hout in enumerate(range(Hout*Wout)):
+            data[cout*Hout*Wout+i] = _d
+    data = data.flatten()
 
-
+    csr_mat = torch.sparse_csr_tensor(crow, cols, data, size=shape_out)
+    return csr_mat 
--- src/sparse.py
+++ src/sparse.py
@@ -1,24 +1,59 @@
 import torch
 from models.conv2d_to_sparse import conv2d_to_sparse as c2s
+from models.conv2d_to_sparse import conv2d_to_sparse2 as c2s2
 from scipy.sparse.linalg import svds as svdnp
 import numpy as np
 import scipy.linalg as linalg
 import scipy
+from torch.nn.functional import conv2d
+from torch.nn import Parameter as P
 
 if __name__ == '__main__':
-    c = torch.nn.Conv2d(2, 3, 3)
+    nc = 3 # n channels
+    kw = 2 # kernel width 
+    kh = 3 # kernel heigh 
+    iw = 4 # image width
+    ih = 4 # image height
+    ns = 1 # n samples
+    cic = nc # conv in channels
+    coc = 2 # conv out channels
+
+    c = torch.nn.Conv2d(cic, coc, (kh, kw))
+    _w = torch.tensor([float(i) for i in range(1,1+nc*coc*kw*kh)]).reshape(coc, nc, kw, kh)
+    c.weight = P(_w, requires_grad=True)
+    
     w = c.weight
     wnp = w.detach().numpy()
+
     b = c.bias
     bnp = b.detach().numpy()
+    print('kernels, bias', w, b)
 
-    x = torch.rand(2, 5, 5)
+    x = torch.rand(ns, nc, ih, iw)
+    print('input shape: ', x.shape)
     xnp = x.detach().numpy()
     r = c(x)
-    print('conv r: ', r)
+    print('conv r: ', r, r.shape)
 
-    csrnp = c2s(x.shape, w, b) 
+    csrnp, csr = c2s(x.shape[1:], w, b, stride=c.stride, padding=c.padding) 
+    #print('csrnp: ', torch.tensor(csrnp.todense()), csrnp.todense().shape)
+    xunp = np.hstack((xnp.flatten(), np.ones(1))) 
+    lcnp = csrnp.todense()
+    runp = lcnp@xunp
+    print('runp: ', torch.tensor(runp).reshape(r.shape))
     
+    #print('csr: ', csr.to_dense(), csr.to_dense().shape)
+    xu = torch.hstack((x.flatten(), torch.ones(1)))
+    lc = csr.to_dense()
+    ru = lc@xu
+    print('ru: ', ru.reshape(r.shape))
+    
+    my_csr = c2s2(x.shape[1:], w, b, stride=c.stride, padding=c.padding)
+    #print('my csr: ', my_csr.to_dense(), my_csr.to_dense().shape)
+    lc = my_csr.to_dense()
+    ru = lc@xu
+    print('my ru: ', ru.reshape(r.shape))
+
     '''
     s = 1000 
     a = torch.zeros(s, s)
@@ -28,8 +63,8 @@ if __name__ == '__main__':
     a[b<0.1] = b[b<0.1]
     #print('input matrix', a)
 
-    # numpy version of matrix a
     anp = a.detach().numpy()
+    # numpy version of matrix a
 
     csrnp = scipy.sparse.csr_matrix(anp)
     #print('numpy csr: ', csrnp)
