3de567796df12e01bf266be853ba06d46c0db876
leandro <leandro.desouzarosa@unibo.it>
2024-12-05 16:36:30 +0100

    view

--- src/peepholes/peepholes.py
+++ src/peepholes/peepholes.py
@@ -184,7 +184,6 @@ class Peepholes:
         fig, axs = plt.subplots(1, n_dss+1, sharex='all', sharey='all', figsize=(4*(1+n_dss), 4))
         
         m_ok, s_ok, m_ko, s_ko = {}, {}, {}, {}
-        dfs = {}
 
         for i, ds_key in enumerate(self._phs.keys()):
             if verbose: print('Evaluating {ds_key}')
@@ -196,30 +195,25 @@ class Peepholes:
             m_ok[ds_key], s_ok[ds_key] = oks.mean(), oks.std()
             m_ko[ds_key], s_ko[ds_key] = kos.mean(), kos.std()
 
-            coks = ['ok' for x in oks]
-            ckos = ['ko' for x in kos]
-            dfs[ds_key] = pd.DataFrame({
-                'score': np.hstack((oks, kos)),
-                'class': np.hstack((coks, ckos))
-                }) 
 
             #--------------- 
             # plotting
             #---------------
-            # plot oks and kos dist over val
             ax = axs[i+1]
-            sb.histplot(data=dfs[ds_key], ax=ax, bins=bins, x='score', hue='class', stat='density', legend=True)
+            sb.histplot(data=pd.DataFrame({'score': oks}), ax=ax, bins=bins, x='score', stat='density', label='ok', alpha=0.5)
+            sb.histplot(data=pd.DataFrame({'score': kos}), ax=ax, bins=bins, x='score', stat='density', label='ko', alpha=0.5)
             ax.set_xlabel(score_type)
             ax.set_ylabel('%')
             ax.title.set_text(ds_key)
+            plt.legend(title='dist')
         
         # plot train and test distributions
         ax = axs[0]
-        sb.histplot(data=dfs['train'], ax=ax, bins=bins, x='score', stat='density', legend=True, alpha=0.5)
-        sb.histplot(data=dfs['val'], ax=ax, bins=bins, x='score', stat='density', legend=True, alpha=0.5)
+        sb.histplot(data=dfs['train'], ax=ax, bins=bins, x='score', stat='density', label='train', alpha=0.5)
+        sb.histplot(data=dfs['val'], ax=ax, bins=bins, x='score', stat='density', label='val', alpha=0.5)
         ax.set_ylabel('%')
         ax.set_xlabel(score_type)
-
+        plt.legend(title='datasets')
         plt.savefig((self.path/self.name).as_posix()+'.png', dpi=300, bbox_inches='tight')
         plt.close()
 
