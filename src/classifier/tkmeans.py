fix normalization
update classifier and phs

src/classifier/tkmeans.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils.data import DataLoader
 
 # torch kmeans
+
 # https://github.com/CSOgroup/torchgmm/tree/main
 from torchgmm.clustering import KMeans as tKMeans
 
@@ -13,15 +14,12 @@ import logging
 logging.getLogger('pytorch_lightning.utilities.rank_zero').setLevel(logging.CRITICAL)
 logging.getLogger("pytorch_lightning.accelerators.cuda").setLevel(logging.CRITICAL)
 
-
-
 class KMeans(ClassifierBase): # quella buona
     def __init__(self, **kwargs):
         cls_kwargs = kwargs.pop('cls_kwargs') if 'cls_kwargs' in kwargs else {}
         ClassifierBase.__init__(self, **kwargs)
 
-        self._classifier = tKMeans(num_clusters=self.nl_class, **cls_kwargs, trainer_params=dict(num_nodes=1, accelerator=self.device.type, devices=[self.device.index]))
-
+        self._classifier = tKMeans(num_clusters=self.nl_class, **cls_kwargs, trainer_params=dict(num_nodes=1, accelerator=self.device.type, devices=[self.device.index], max_epochs=5000, enable_progress_bar=True))
 
     def fit(self, **kwargs):
         '''

src/coreVectors/activations.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 def get_activations(self, **kwargs):
     self.check_uncontexted()
     model = self._model
-    device = self.device 
+    device = self._model.device 
     hooks = model.get_hooks()
 
     bs = kwargs['batch_size'] if 'batch_size' in kwargs else 64
@@ -27,11 +27,11 @@ def get_activations(self, **kwargs):
 
         if file_path.exists():
             if verbose: print(f'File {file_path} exists. Loading from disk.')
-            self._actds[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r+').to(self.device)
+            self._actds[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r+')
             if verbose: print(f'loaded {file_path}')
         else:
             if verbose: print(f'creating {file_path}') 
-            self._actds[ds_key] = PersistentTensorDict(filename=file_path, batch_size=[n_samples], device=self.device, mode='w')
+            self._actds[ds_key] = PersistentTensorDict(filename=file_path, batch_size=[n_samples], mode='w')
 
         #------------------------------------------------
         # pre-allocate predictions, results, activations
@@ -100,19 +100,19 @@ def get_activations(self, **kwargs):
         
         for cvs_data, act_data in tqdm(zip(cvs_dl, act_dl), disable=not verbose, total=len(cvs_dl)):
             with torch.no_grad():
-                y_predicted = model(cvs_data['image'])
+                y_predicted = model(cvs_data['image'].to(device))
             
             # do not save predictions and results if it is already there
             if not has_pred:
-                predicted_labels = y_predicted.argmax(axis = 1)
+                predicted_labels = y_predicted.argmax(axis = 1).cpu()
                 cvs_data['pred'] = predicted_labels
                 cvs_data['result'] = predicted_labels == cvs_data['label']
             
             for lk in _layers_to_save:
                 if model._si:
-                    act_data['in_activations'][lk] = hooks[lk].in_activations[:]
+                    act_data['in_activations'][lk] = hooks[lk].in_activations[:].cpu()
 
                 if model._so:
-                    act_data['out_activations'][lk] = hooks[lk].out_activations[:]
+                    act_data['out_activations'][lk] = hooks[lk].out_activations[:].cpu()
     return 
 

src/coreVectors/coreVectors.py
@@ -20,7 +20,6 @@ class CoreVectors():
         self.path.mkdir(parents=True, exist_ok=True)
 
         self._model = kwargs['model'] if 'model' in kwargs else None  
-        self.device = kwargs['device'] if 'device' in kwargs else 'cpu'  
 
         # computed in get_coreVec_dataset()
         self._cvs_file_paths = {} 
@@ -75,7 +74,7 @@ class CoreVectors():
                 if not lk in is_normed[ds_key]:
                     layers_to_norm[ds_key].append(lk)
                     cnt += 1
-        print('Layers to norm: ', layers_to_norm)
+        if verbose: print('Layers to norm: ', layers_to_norm)
 
         if cnt == 0:
             if verbose: print('All corevectors seems to be normalized. Doing nothing')
@@ -90,14 +89,12 @@ class CoreVectors():
         # TODO: It is excessive to renormalize all layers again (including the ones already normalized). Gotta change the logic to normalize only the ones in `layers_to_norm` 
         for ds_key in self._corevds:
             if verbose: print(f'\n ---- Normalizing core vectors for {ds_key}\n')
-            td = self._corevds[ds_key]['coreVectors']  
-            dl = DataLoader(td, batch_size=bs, collate_fn=lambda x: x)
+            dl = DataLoader(self._corevds[ds_key], batch_size=bs, collate_fn=lambda x: x)
+            
             for batch in tqdm(dl, disable=not verbose, total=len(dl)):
-                n_in = len(batch) 
-                batch = (batch - means)/stds
+                batch['coreVectors'] = (batch['coreVectors'] - means)/stds
 
             is_normed[ds_key] = list(set(layers_to_norm[ds_key]).union(is_normed[ds_key])) 
-        print('is normed: ', is_normed)
 
         if not file_path.exists(): torch.save((means, stds, is_normed, wrt), file_path)
         self._norm_mean = means
@@ -148,7 +145,7 @@ class CoreVectors():
             self._cvs_file_paths[ds_key] = file_path
             
             if verbose: print(f'File {file_path} exists. Loading from disk.')
-            self._corevds[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r').to(self.device)
+            self._corevds[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r')
 
             self._n_samples[ds_key] = len(self._corevds[ds_key])
             if verbose: print('loaded n_samples: ', self._n_samples[ds_key])

src/coreVectors/svd_coreVectors.py
@@ -16,7 +16,7 @@ def get_coreVectors(self, **kwargs):
     self.check_uncontexted()
     
     model = self._model 
-    device = model.device 
+    device = self._model.device 
     normalize_wrt = kwargs['normalize_wrt'] if 'normalize_wrt' in kwargs else None 
     verbose = kwargs['verbose'] if 'verbose' in kwargs else False
 
@@ -82,10 +82,10 @@ def get_coreVectors(self, **kwargs):
                     n_act = act_data['in_activations'][lk].shape[0]
                     acts = act_data['in_activations'][lk]
                     acts_flat = acts.flatten(start_dim=1)
-                    ones = torch.ones(n_act, 1).to(device)
-                    _acts = torch.hstack((acts_flat, ones))
+                    ones = torch.ones(n_act, 1)
+                    _acts = torch.hstack((acts_flat, ones)).to(device)
                     phs = (reduct_m@_acts.T).T
-                    cvs_data['coreVectors'][lk] = phs
+                    cvs_data['coreVectors'][lk] = phs.cpu()
             if isinstance(layer, torch.nn.Conv2d):
                 pad_mode = layer.padding_mode if layer.padding_mode != 'zeros' else 'constant'
                 padding = _reverse_repeat_tuple(layer.padding, 2) 
@@ -95,9 +95,9 @@ def get_coreVectors(self, **kwargs):
                     acts_pad = pad(acts, pad=padding, mode=pad_mode)
 
                     acts_flat = acts_pad.flatten(start_dim=1)
-                    ones = torch.ones(n_act, 1).to(device)
-                    _acts = torch.hstack((acts_flat, ones))
+                    ones = torch.ones(n_act, 1)
+                    _acts = torch.hstack((acts_flat, ones)).to(device)
                     phs = (reduct_m@_acts.T).T
-                    cvs_data['coreVectors'][lk] = phs
+                    cvs_data['coreVectors'][lk] = phs.cpu()
 
     return        

src/peepholes/peepholes.py
@@ -2,8 +2,12 @@
 from pathlib import Path
 from tqdm import tqdm
 from collections import Counter
-from matplotlib import pyplot as plt
 import numpy as np
+
+# plotting stuff
+from matplotlib import pyplot as plt
+import seaborn as sb
+import pandas as pd
 
 # torch stuff
 import torch
@@ -48,24 +52,24 @@ class Peepholes:
             raise RuntimeError('No prediction probabilities. Please run classifier.compute_empirical_posteriors() first.')
         _empp = self._classifier._empp.to(self.device)
         verbose = kwargs['verbose'] if 'verbose' in kwargs else False
-        _dls = kwargs['loaders']
+        dls = kwargs['loaders']
 
         layer = self.layer 
 
-        for ds_key in _dls:
-            bs = _dls[ds_key].batch_size
+        for ds_key in dls:
+            bs = dls[ds_key].batch_size
             if verbose: print(f'\n ---- Getting peepholes for {ds_key}\n')
             file_path = self.path/(self.name.name+'.'+ds_key)
            
             if file_path.exists():
                 if verbose: print(f'File {file_path} exists. Loading from disk.')
-                self._phs[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r+').to(self.device)
+                self._phs[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r+')
                 n_samples = len(self._phs[ds_key])
                 if verbose: print('loaded n_samples: ', n_samples)
             else:
-                n_samples = len(_dls[ds_key].dataset)
+                n_samples = len(dls[ds_key].dataset)
                 if verbose: print('loader n_samples: ', n_samples) 
-                self._phs[ds_key] = PersistentTensorDict(filename=file_path, batch_size=[n_samples], device=self.device, mode='w')
+                self._phs[ds_key] = PersistentTensorDict(filename=file_path, batch_size=[n_samples], mode='w')
             
             #-----------------------------------------
             # Pre-allocate peepholes
@@ -79,13 +83,13 @@ class Peepholes:
                 # computing peepholes
                 #-----------------------------------------
                 if verbose: print('\n ---- computing peepholes \n')
-                _dl_t = DataLoader(self._phs[ds_key], batch_size=bs, collate_fn=lambda x:x)
-                for batch in tqdm(zip(_dls[ds_key], _dl_t), disable=not verbose, total=len(_dl_t)):
+                dl_t = DataLoader(self._phs[ds_key], batch_size=bs, collate_fn=lambda x:x)
+                for batch in tqdm(zip(dls[ds_key], dl_t), disable=not verbose, total=len(dl_t)):
                     data_in, data_t = batch
                     cp = self._classifier.classifier_probabilities(batch=data_in, verbose=verbose).to(self.device)
-                    _lp = cp@_empp
-                    _lp /= _lp.sum(dim=1, keepdim=True)
-                    data_t[layer]['peepholes'] = _lp
+                    lp = cp@_empp
+                    lp /= lp.sum(dim=1, keepdim=True)
+                    data_t[layer]['peepholes'] = lp.cpu()
             else:
                 if verbose: print('Peepholes for {layer} already present. Skipping.')
         return 
@@ -161,9 +165,63 @@ class Peepholes:
             file_path = self.path/(self.name.name+'.'+ds_key)
            
             if verbose: print(f'File {file_path} exists. Loading from disk.')
-            _phs[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r').to(self.device)
+            self._phs[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r')
         
         return
+    
+    def evaluate_dists(self, **kwargs):
+        self.check_uncontexted()
+         
+        layer = self.layer 
+        verbose = kwargs['verbose'] if 'verbose' in kwargs else False 
+        cv_dls = kwargs['coreVectors']
+        score_type = kwargs['score_type']
+        bins = kwargs['bins'] if 'bins' in kwargs else 100
+
+        print('\n-------------\nEvaluating Distributions\n-------------\n') 
+        
+        n_dss = len(self._phs.keys())
+        fig, axs = plt.subplots(1, n_dss+1, sharex='all', sharey='all', figsize=(4*(1+n_dss), 4))
+        
+        m_ok, s_ok, m_ko, s_ko = {}, {}, {}, {}
+
+        for i, ds_key in enumerate(self._phs.keys()):
+            if verbose: print('Evaluating {ds_key}')
+            results = cv_dls[ds_key].dataset['result']
+            scores = self._phs[ds_key][layer]['score_'+score_type]
+            oks = (scores[results == True]).detach().cpu().numpy()
+            kos = (scores[results == False]).detach().cpu().numpy()
+
+            m_ok[ds_key], s_ok[ds_key] = oks.mean(), oks.std()
+            m_ko[ds_key], s_ko[ds_key] = kos.mean(), kos.std()
+
+
+            #--------------- 
+            # plotting
+            #---------------
+            ax = axs[i+1]
+            sb.histplot(data=pd.DataFrame({'score': oks}), ax=ax, bins=bins, x='score', stat='density', label='ok n=%d'%len(oks), alpha=0.5)
+            sb.histplot(data=pd.DataFrame({'score': kos}), ax=ax, bins=bins, x='score', stat='density', label='ko n=%d'%len(kos), alpha=0.5)
+            ax.set_xlabel('score: '+score_type)
+            ax.set_ylabel('%')
+            ax.title.set_text(ds_key)
+            ax.legend(title='dist')
+        
+        # plot train and test distributions
+        ax = axs[0]
+        scores = self._phs['train'][layer]['score_'+score_type].detach().cpu().numpy()
+        sb.histplot(data=pd.DataFrame({'score': scores}), ax=ax, bins=bins, x='score', stat='density', label='train n=%d'%len(scores), alpha=0.5)
+        scores = self._phs['val'][layer]['score_'+score_type].detach().cpu().numpy()
+        sb.histplot(data=pd.DataFrame({'score': scores}), ax=ax, bins=bins, x='score', stat='density', label='val n=%d'%len(scores), alpha=0.5)
+        ax.set_ylabel('%')
+        ax.set_xlabel('score: '+score_type)
+        ax.legend(title='datasets')
+
+        plt.savefig((self.path/self.name).as_posix()+'.png', dpi=300, bbox_inches='tight')
+        plt.close()
+
+        if verbose: print('oks mean, std, n: ', m_ok, s_ok, len(oks), '\nkos, mean, std, n', m_ko, s_ko, len(kos))
+        return m_ok, s_ok, m_ko, s_ko
 
     def evaluate(self, **kwargs): 
         self.check_uncontexted()

src/xp_tuning.py
@@ -1,6 +1,5 @@
 # python stuff
 from pathlib import Path as Path
-from numpy.random import randint
 
 # Our stuff
 from datasets.cifar import Cifar
@@ -8,8 +7,6 @@ from models.model_wrap import ModelWrap
 from coreVectors.coreVectors import CoreVectors 
 from coreVectors.svd_coreVectors import reduct_matrices_from_svds as parser_fn
 from classifier.classifier_base import trim_corevectors
-from classifier.kmeans import KMeans 
-from classifier.gmm import GMM 
 from classifier.tkmeans import KMeans as tKMeans 
 from classifier.tgmm import GMM as tGMM 
 from peepholes.peepholes import Peepholes
@@ -17,6 +14,7 @@ from peepholes.peepholes import Peepholes
 # torch stuff
 import torch
 from torchvision.models import vgg16, VGG16_Weights
+from cuda_selector import auto_cuda
 
 # Tuner
 import tempfile
@@ -59,18 +57,26 @@ def peephole_wrap(config, **kwargs):
     g = list(ph_path.glob(f'{ph_config_name}.*')) 
     if len(g) > 0:
         print('Already run this configuration, skipping peepholes computation')
-        ph = Peepholes(
+        peepholes = Peepholes(
                 path = ph_path,
                 name = ph_config_name,
                 classifier = None,
                 layer = target_layer,
                 device = device
                 )
-        
-        ph.load_only(
-                loaders = ['train', 'test', 'val'],
-                verbose = True
+        with peepholes as ph: 
+            ph.load_only(
+                    loaders = ['train', 'test', 'val'],
+                    verbose = True
+                    )
+            
+            # TODO: should save and load these instead of running the function again
+            mok, sok, mko, sko = ph.evaluate_dists(
+                score_type = score_type,
+                coreVectors = cv_dl,
+                bins = 20
                 )
+
     else:
         cls = tGMM(
                 nl_classifier = n_cls,
@@ -84,25 +90,30 @@ def peephole_wrap(config, **kwargs):
         cls.fit(dataloader = cv_dl['train'], verbose=True)
         cls.compute_empirical_posteriors(verbose=True)
                                                                      
-        ph = Peepholes(
+        peepholes = Peepholes(
                 path = ph_path,
                 name = ph_config_name,
                 classifier = cls,
                 layer = target_layer,
                 device = device
                 )
-
-        ph.get_peepholes(
+        
+        with peepholes as ph:
+            ph.get_peepholes(
                 loaders = cv_dl,
                 verbose = True
                 )
     
-        ph.get_scores(verbose=True)
+            ph.get_scores(
+                    batch_size = 512, 
+                    verbose=True
+                    )
 
-    cok, cko = ph.evaluate(
-            score_type = score_type,
-            coreVectors = cv_dl
-            )
+            mok, sok, mko, sko = ph.evaluate_dists(
+                score_type = score_type,
+                coreVectors = cv_dl,
+                bins = 20
+                )
 
     with tempfile.TemporaryDirectory() as checkpoint_dir:
         data_path = Path(checkpoint_dir) / "tune_checkpoint.pkl"
@@ -111,8 +122,10 @@ def peephole_wrap(config, **kwargs):
                                                                
         checkpoint = Checkpoint.from_directory(checkpoint_dir)
         train.report({
-            "cok": cok,
-            'cko': cok,
+            'mok': mok['val'],
+            'sok': sok['val'],
+            'mko': mko['val'],
+            'sko': sko['val'],
             },
             checkpoint=checkpoint
         )
@@ -121,8 +134,7 @@ def peephole_wrap(config, **kwargs):
 
 if __name__ == "__main__":
     use_cuda = torch.cuda.is_available()
-    cuda_index = torch.cuda.device_count() - 3
-    device = torch.device(f"cuda:{cuda_index}" if use_cuda else "cpu")
+    device = torch.device(auto_cuda('utilization')) if use_cuda else torch.device("cpu")
     print(f"Using {device} device")
 
     #--------------------------------
@@ -134,10 +146,10 @@ if __name__ == "__main__":
     pretrained = True
     dataset = 'CIFAR100' 
     seed = 29
-    bs = 64
+    bs = 512 
     model_dir = '/srv/newpenny/XAI/models'
     model_name = f'vgg16_pretrained={pretrained}_dataset={dataset}-'\
-    f'augmented_policy=CIFAR10_bs={bs}_seed={seed}.pth'
+    f'augmented_policy=CIFAR10_bs=64_seed={seed}.pth'
     
     svds_name = 'svds' 
     svds_path = Path.cwd()/'../data'
@@ -151,81 +163,75 @@ if __name__ == "__main__":
     #--------------------------------
     # CoreVectors 
     #--------------------------------
-    cv = CoreVectors(
+    corevecs = CoreVectors(
             path = cvs_path,
             name = cvs_name,
-            model = None 
             )
-    
-    # load corevds 
-    cv.load_only(
-            loaders = ['train', 'test', 'val'],
-            verbose = True
-            ) 
 
-    cv_dl = cv.get_dataloaders(verbose=True)
-    i = 0
-    print('\nPrinting some corevecs')
-    for data in cv_dl['val']:
-        print(data['coreVectors']['classifier.0'])
-        i += 1
-        if i == 3: break
+    with corevecs as cv: 
+        # load corevds 
+        cv.load_only(
+                loaders = ['train', 'test', 'val'],
+                verbose = True
+                ) 
 
-    #--------------------------------
-    # Tunning 
-    #--------------------------------
+        cv_dl = cv.get_dataloaders(batch_size=bs, verbose=True)
+        i = 0
 
-    config = {
-            'peep_size': tune.choice([2**i for i in range(2, 9)]),
-            'n_classifier': tune.choice([2**i for i in range(2, 9)]),
-            'score_type': tune.choice(['max', 'entropy']), 
-            }
+        #--------------------------------
+        # Tunning 
+        #--------------------------------
 
-    if device == 'cpu':
-        resources = {"cpu": 1}
-    else:
-        resources = {"cpu": 16, "gpu": 5}
+        config = {
+                'peep_size': tune.choice([20*i for i in range(2, 16)]),
+                'n_classifier': tune.choice([20*i for i in range(2, 16)]),
+                #'score_type': tune.choice(['max', 'entropy']), 
+                'score_type': tune.choice(['entropy']), 
+                }
 
-    hyper_params_file = phs_path/f'hyperparams.pickle'
-    if hyper_params_file.exists():
-        print("Already tunned parameters fount in %s. Skipping"%(hyper_params_file.as_posix()))
-        exit()
+        if device == 'cpu':
+            resources = {"cpu": 32}
+        else:
+            resources = {"cpu": 32, "gpu": 5}
+
+        hyper_params_file = phs_path/f'hyperparams.pickle'
+        if hyper_params_file.exists():
+            print("Already tunned parameters fount in %s. Skipping"%(hyper_params_file.as_posix()))
+            exit()
    
-    searcher = OptunaSearch(
-            metric = ['cok', 'cko'],
-            mode = ['max', 'max']
-            )
-    algo = ConcurrencyLimiter(searcher, max_concurrent=4)
-    scheduler = AsyncHyperBandScheduler(grace_period=5, max_t=100, metric="cok", mode="max") 
-    tune_storage_path = Path.cwd()/'../data/tuning'
-    trainable = tune.with_resources(
-            partial(
-                peephole_wrap,
-                device = device,
-                corevec_dataloader = cv_dl,
-                ph_path = phs_path,
-                ph_name = phs_name
-                ),
-            resources 
-            )
+        searcher = OptunaSearch(
+                metric = ['mok', 'sok', 'mko', 'sko'],
+                mode = ['max', 'min', 'min', 'min']
+                )
+        algo = ConcurrencyLimiter(searcher, max_concurrent=4)
+        scheduler = AsyncHyperBandScheduler(grace_period=5, max_t=100, metric="mok", mode="max") 
+        tune_storage_path = Path.cwd()/'../data/tuning'
+        trainable = tune.with_resources(
+                partial(
+                    peephole_wrap,
+                    device = device,
+                    corevec_dataloader = cv_dl,
+                    ph_path = phs_path,
+                    ph_name = phs_name
+                    ),
+                resources 
+                )
 
-    tuner = tune.Tuner(
-            trainable,
-            tune_config = tune.TuneConfig(
-                search_alg = algo,
-                num_samples = 15, 
-                scheduler = scheduler,
-                ),
-            run_config = train.RunConfig(
-                storage_path = tune_storage_path
-                ),
-            param_space = config,
-            )
-    result = tuner.fit()
+        tuner = tune.Tuner(
+                trainable,
+                tune_config = tune.TuneConfig(
+                    search_alg = algo,
+                    num_samples = 50, 
+                    scheduler = scheduler,
+                    ),
+                run_config = train.RunConfig(
+                    storage_path = tune_storage_path
+                    ),
+                param_space = config,
+                )
+        result = tuner.fit()
 
-    results_df = result.get_dataframe()
-    print('results: ', results_df)
-    results_df.to_pickle(hyper_params_file)
-    
-    # TODO: use GPU for peephole computations
-
+        results_df = result.get_dataframe()
+        print('results: ', results_df)
+        results_df.to_pickle(hyper_params_file)
+        